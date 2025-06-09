c0197f72ca9287e58b53e5c0daf47fe86cd88e8f
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-06-09 13:09:58 -0600

    Move restaurant info constants into object

--- src/home_page.js
+++ src/home_page.js
@@ -1,16 +1,17 @@
-const RESTAURANT_NAME = "Eyes of the Heart Eatery";
-const RESTAURANT_DESCRIPTION =
-  "This Super Special Restaurant is wonderful with amazing food.";
-const RESTAURANT_HOURS = [
-  "Sunday: 8am - 8pm",
-  "Monday: 8am - 6pm",
-  "Tuesday: 8am - 6pm",
-  "Wednesday: 8am - 6pm",
-  "Thursday: 8am - 6pm",
-  "Friday: 8am - 10pm",
-  "Saturday: 8am - 10pm",
-];
-const RESTAURANT_ADDRESS = "123 12th Street, Mountainville, CO 12345";
+const RESTAURANT_INFO = {
+  NAME: "Eyes of the Heart Eatery",
+  DESCRIPTION: "This Super Special Restaurant is wonderful with amazing food.",
+  HOURS: [
+    "Sunday: 8am - 8pm",
+    "Monday: 8am - 6pm",
+    "Tuesday: 8am - 6pm",
+    "Wednesday: 8am - 6pm",
+    "Thursday: 8am - 6pm",
+    "Friday: 8am - 10pm",
+    "Saturday: 8am - 10pm",
+  ],
+  ADDRESS: "123 12th Street, Mountainville, CO 12345",
+};
 
 export default function loadHomePage() {
   const restaurantHeading = createRestaurantHeading();
@@ -28,14 +29,14 @@ export default function loadHomePage() {
 function createRestaurantHeading() {
   const heading = document.createElement("h1");
   heading.classList.add("content__box");
-  heading.textContent = RESTAURANT_NAME;
+  heading.textContent = RESTAURANT_INFO.NAME;
   return heading;
 }
 
 function createRestaurantDescription() {
   const description = document.createElement("p");
   description.classList.add("content__box");
-  description.textContent = RESTAURANT_DESCRIPTION;
+  description.textContent = RESTAURANT_INFO.DESCRIPTION;
   return description;
 }
 
@@ -47,7 +48,7 @@ function createRestaurantHours() {
   hoursSection.classList.add("content__box");
   hoursHeading.textContent = "Hours";
 
-  for (const hours of RESTAURANT_HOURS) {
+  for (const hours of RESTAURANT_INFO.HOURS) {
     const hoursItem = document.createElement("li");
     hoursItem.textContent = hours;
     hoursList.appendChild(hoursItem);
@@ -66,7 +67,7 @@ function createRestaurantAddress() {
 
   addressSection.classList.add("content__box");
   addressHeading.textContent = "Address";
-  address.textContent = RESTAURANT_ADDRESS;
+  address.textContent = RESTAURANT_INFO.ADDRESS;
 
   addressSection.appendChild(addressHeading);
   addressSection.appendChild(address);
